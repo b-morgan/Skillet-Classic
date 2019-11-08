2ca00fed24160ee790f3d4d0373b91bece92d845
bsmorgan <bsmorgan@d8a62c8f-5bc8-461e-97e9-793d77676411>
2019-11-08 14:59:49 +0000

    - Fix for issues #117, 119, 121, 122; Inventory.lua missing PT declaration. - Fix for issue #118; Preserve Custom Groups

--- trunk/Inventory.lua
+++ trunk/Inventory.lua
@@ -16,6 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ]]--
+local PT = LibStub("LibPeriodicTable-3.1")
 
 --
 -- recursive reagent craftability check
