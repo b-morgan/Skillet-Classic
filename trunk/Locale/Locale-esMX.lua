d8479f93369b7943f88e79bf8c76ab57ffe009f7
bsmorgan <bsmorgan@d8a62c8f-5bc8-461e-97e9-793d77676411>
2020-04-17 13:19:41 +0000

    - Update Localizations

---
+++ trunk/Locale/Locale-esMX.lua
@@ -0,0 +1,21 @@
+--[[
+ Skillet: A tradeskill window replacement.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
+local L = LibStub("AceLocale-3.0"):NewLocale("Skillet", "esMX")
+if not L then return end
+
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="english", handle-subnamespaces="concat")@
