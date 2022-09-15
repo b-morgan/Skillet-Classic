2a637b7010f23bafcd8f10ef66f0d5877bc26ba7
Brad Morgan <brad-morgan@comcast.net>
2022-09-14 18:54:04 -0600

    Fix spelling in comment

--- TradeskillInfo.lua
+++ TradeskillInfo.lua
@@ -167,6 +167,8 @@ function Skillet:GetTradeSkillItemLink(index)
 	if recipe then
 		if self.isCraft then
 			link = GetCraftItemLink(index)
+		elseif self.currentTrade == 7411 and recipe.itemID == 0 then
+			link = GetTradeSkillRecipeLink(index)
 		else
 			_, link = GetItemInfo(recipe.itemID)
 		end
