edcfc5879a593118015dda146e7e8f896d1d0cb1
Brad Morgan <brad-morgan@comcast.net>
2024-09-12 08:02:03 -0600

    Separate SkillLevelData.lua into two files

--- SkilletNewsData.lua
+++ SkilletNewsData.lua
@@ -26,6 +26,15 @@ local isCata = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
 
 Skillet.NewsName = "Skillet-Classic News"
 Skillet.NewsData = {
+	{	version = "1.99",
+		data = {
+			{	name = "Changes",
+				data = {
+					{ header = "Skill Levels", body = "Separate skill level data into two files" },
+				},
+			},
+		},
+	},
 	{	version = "1.98",
 		data = {
 			{	name = "Fixes",
