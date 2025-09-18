d747c80fa5b8719d1bae67a758937ed8a1bf8745
Brad Morgan <brad-morgan@comcast.net>
2025-09-18 07:10:56 -0600

    Fix tcopy

--- SkilletQueue.lua
+++ SkilletQueue.lua
@@ -827,9 +827,12 @@ function Skillet:QueueMoveToBottom(index)
 end
 
 local function tcopy(t)
-  local u = { }
-  for k, v in pairs(t) do u[k] = v end
-  return setmetatable(u, getmetatable(t))
+	local u = {}
+	if t then
+		for k, v in pairs(t) do u[k] = v end
+		return setmetatable(u, getmetatable(t))
+	end
+	return {}
 end
 
 function Skillet:SaveQueue(name, overwrite)
