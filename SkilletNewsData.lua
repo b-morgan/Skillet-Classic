5aa33c07c202226eb1bad54a7ab09966a93ddfbd
Brad Morgan <brad-morgan@comcast.net>
2025-10-21 08:22:45 -0600

    Update Vanilla TOC

--- SkilletNewsData.lua
+++ SkilletNewsData.lua
@@ -26,6 +26,15 @@ local isCata = WOW_PROJECT_ID == WOW_PROJECT_CATACLYSM_CLASSIC
 
 Skillet.NewsName = "Skillet-Classic News"
 Skillet.NewsData = {
+	{	version = "2.20",
+		data = {
+			{	name = "Changes",
+				data = {
+					{ header = "TOC", body = "Update TOC" },
+				},
+			},
+		},
+	},
 	{	version = "2.19",
 		data = {
 			{	name = "Changes",
